21e473b43f0fab0f30b9d6cafaa021f0e2b6901e
tiagotba <osbentoalves@gmail.com>
2019-04-08 17:26:37 -0300

    Add solution with problem resolution class

---
+++ ClassLibraryUtils/DynamicFlattenArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryUtils
+{
+    public static class DynamicFlattenArray
+    {
+        private static void Flatten(dynamic item, List<int> flattened)
+        {
+            Random random = new Random();
+            dynamic array = new dynamic[random.Next(3, 10)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (random.NextBool())
+                {
+                    array[i] = new dynamic[random.Next(3, 10)];
+
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        if (random.NextBool())
+                        {
+                            array[i][j] = random.Next(1, 100);
+                        }
+                        else
+                        {
+                            array[i][j] = new int[random.Next(3, 10)];
+
+                            for (int k = 0; k < array[i][j].Length; k++)
+                            {
+                                array[i][j][k] = random.Next(1000);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    array[i] = random.Next(1, 100);
+                }
+            }
+        }
+
+        public static bool NextBool(this Random random)
+        {
+            return random.Next(0, 2) == 0;
+        }
+
+    }
+
+    
+}
